7add1e82d9643f4db7c9f751ab2958d4b404942b
rainpasttime <2923336451@qq.com>
2017-12-02 16:07:45 +0800

    finish

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-const main = require('./main/main');
+"use strict";
+describe("spec",function(){
+    it("spec one smaller than 8Km waiting 5 minutes",function(){
+        const main = require('./main/main');
+        let inputs = {running:"3",waiting:"5"};
+        let result = main(inputs);
+        expect(result).toEqual(8);
+    });
 
-main();
+    it("spec two bigger than 9Km waiting 15 minutes",function(){
+        const main = require('./main/main');
+        let inputs = {running:"9",waiting:"15"};
+        let result = main(inputs);
+        expect(result).toEqual(16);
+    });
+
+    it("spec three smaller than 8Km waiting 0 minutes",function(){
+        const main = require('./main/main');
+        let inputs = {running:"2",waiting:"0"};
+        let result = main(inputs);
+        expect(result).toEqual(6);
+    });
+
+    it("spec four bigger than 9Km waiting 0 minutes",function(){
+        const main = require('./main/main');
+        let inputs = {running:"15",waiting:"0"};
+        let result = main(inputs);
+        expect(result).toEqual(22);
+    });
+
+    it("spec five smaller than 2Km waiting 15 minutes",function(){
+        const main = require('./main/main');
+        let inputs = {running:"1",waiting:"15"};
+        let result = main(inputs);
+        expect(result).toEqual(9);
+    });
+
+    it("spec six smaller than 2Km waiting 0 minutes",function(){
+        const main = require('./main/main');
+        let inputs = {running:"1",waiting:"0"};
+        let result = main(inputs);
+        expect(result).toEqual(6);
+    });
+});
--- main/main.js
+++ main/main.js
@@ -1,4 +1,25 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+"use strict";
+module.exports = function main(inputs) {
+    let kilometer = inputs.running;
+    let minutes = inputs.waiting;
+    let sum = 0;
+
+    if(kilometer>0 && kilometer<=2)
+    {
+        sum = 6+minutes*0.25;
+        sum = Math.floor(sum);
+        return sum;
+    }
+    else if(kilometer>2 && kilometer<=8)
+    {
+        sum = 6+(kilometer-2)*0.8+minutes*0.25;
+        sum = Math.floor(sum);
+        return sum;
+    }
+    else
+    {
+        sum = 6+6*0.8+(kilometer-8)*1.6+minutes*0.25;
+        sum = Math.floor(sum);
+        return sum;
+    }
 };
